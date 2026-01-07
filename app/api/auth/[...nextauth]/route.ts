9d527474ece4ea8b244dab6cd0e14bc9a478bfbe
devitgg99 <suerporlong123@gmail.com>
2026-01-07 20:22:29 +0700

    done

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL  ;
+const baseUrl = process.env.NEXT_PUBLIC_API_URL  ;
 
 export const authOptions = {
   providers: [
