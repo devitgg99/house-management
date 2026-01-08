43fb9d0c494b9cd947c1ca8c3ba461de4eccd732
devitgg99 <suerporlong123@gmail.com>
2026-01-08 15:38:47 +0700

    done change

--- actions/auth/RegisterAction.ts
+++ actions/auth/RegisterAction.ts
@@ -1,3 +1,4 @@
+"use server";
 
 import { RegisterService } from "@/services/auth.service";
 import { RegisterRequest } from "@/types/auth/Register";
@@ -5,5 +6,5 @@ import { RegisterRequest } from "@/types/auth/Register";
 export const RegisterAction = async (registerData: RegisterRequest) => {
   const response = await RegisterService(registerData);
   return response;
-
+  
 };
